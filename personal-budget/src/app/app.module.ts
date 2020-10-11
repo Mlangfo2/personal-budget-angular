2ee91d05ee0b697c11b45b3628582c5f9eeb8dd1
Micah Langford <48960550+Mlangfo2@users.noreply.github.com>
2020-10-10 21:02:38 -0400

    Pushed what I had

---
+++ personal-budget/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeroComponent } from './hero/hero.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ArticleComponent } from './article/article.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HeroComponent,
+    HomepageComponent,
+    ArticleComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
